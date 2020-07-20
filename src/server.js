ee77fd614fd904081647fe0c567f6c07b9ecd4cd
Simon Wilson <simon@t0day.com>
2020-07-20 09:01:40 +0100

    Exclude Zara and tweak messaging

--- src/server.js
+++ src/server.js
@@ -110,8 +110,7 @@ const openConversation = async (users) => {
 const sendPairingMessages = async (channel, isThreesome = false) => {
   const sendMessage = (text) => web.chat.postMessage({ channel, text });
   await sendMessage(`Hi! You have been ${isThreesome ? "trupled" : "paired"} at random this week for a virtual coffee. :computer: :coffee:`);
-  await sendMessage("Feel free to arrange something social that suits you both, as brief or as deep as you like.");
-  await sendMessage("Have fun!");
+  await sendMessage(`Feel free to arrange something social that suits you ${isThreesome ? "all" : "both"}, as brief or as deep as you like. Have fun!`);
 }
 
 app.post("/execute", async (req, res) => {
@@ -138,7 +137,10 @@ app.post("/execute", async (req, res) => {
     allMembers = memberIDs.map(m => ({ id: m, name: memberName(m)}));
 
     // Remove any blacklisted (e.g. furloughed) members
-    filteredMembers = allMembers.filter(m => m.name != "Gjokica Zafirovski");
+    filteredMembers = allMembers.filter(m => ![
+      "Gjokica Zafirovski",
+      "Zara Hillary",
+    ].includes(m.name));
 
     // Pair members completely at random!
     shuffle(filteredMembers);
